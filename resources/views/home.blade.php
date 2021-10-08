6e3303d5cd49c5349575fcc771210c93cfa2ab6e
AntonBohn <anton.bohn@tudock.de>
2021-10-08 16:00:48 +0200

    iconify moved to the top

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,9 +2,9 @@
 
 @section('content')
     <header>
+        <span class="iconify" data-icon="mdi-nuke" ></span>
         @include('components.hero', ['header' => 'Nuke Ware Store'])
     </header>
-    <span class="iconify" data-icon="mdi-nuke"></span>
     <h2>Für Sie Empfohlen</h2>
     <ul class="product-list">
         @foreach($products as $product)
