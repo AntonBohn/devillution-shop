39764e6d65cfa826511d44d9acf46260df88c8e6
AntonBohn <anton.bohn@tudock.de>
2021-10-19 12:43:11 +0200

    edited the web.php

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home', ['pageTitle' => 'Home', 'products' => Product::all()->take(6)]);
+    return view('home', [
+        'pageTitle' => 'Home',
+        'products' => Product::all()->take(6),
+        'bestsellers' => Product::all()->take(6)
+    ]);
 });
 
 Route::get('/playground', function () {
