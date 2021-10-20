58048040d02267114391eb2c0cb47171b6dfa4a9
AntonBohn <anton.bohn@tudock.de>
2021-10-20 16:43:18 +0200

    fix: add viewport matatag

--- resources/views/layout/html-head.blade.php
+++ resources/views/layout/html-head.blade.php
@@ -1,3 +1,4 @@
 <title>{{ $pageTitle }}</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 <script defer src="{{ mix('js/app.js') }}"></script>
