fd3926e620251dfa5529578cf278deb775bab47d
AntonBohn <anton.bohn@tudock.de>
2021-10-25 09:12:45 +0200

    Created the ShortDiscription

--- database/migrations/2021_10_25_070210_add_short_descripionto_product_table.php
+++ database/migrations/2021_10_25_070210_add_short_descripionto_product_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddImagestoProductsTable extends Migration
+class AddShortDescripiontoProductTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,8 +14,7 @@ class AddImagestoProductsTable extends Migration
     public function up()
     {
         Schema::table('products', function (Blueprint $table) {
-            $table->string('images');
-            $table->string('description');
+            $table->string('ShortDescription');
         });
     }
 
