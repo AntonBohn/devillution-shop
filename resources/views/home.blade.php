cb2b3b1b4196664d39a430dfd17a729ed3049a8a
AntonBohn <anton.bohn@tudock.de>
2021-10-28 17:06:41 +0200

    feat(home-text): added the home-text

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,23 @@
     @include('components.hero', ['header' => 'Lorem ipsum dolor sit amet'])
     <main>
         @include('components.catalog.product.mini-list', ['title' => 'Für Sie empfohlen', 'products' => $products])
+        <div class="text-content">
+            <h3 class="content-headline">Lorem ipsum dolor sit amet.</h3>
+            <div class="content-text">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ad debitis deserunt nisi quam qui totam!
+                    Adipisci animi consequatur corporis earum error exercitationem explicabo harum ipsa iure nisi nulla
+                    perspiciatis provident quibusdam quod, repellat repellendus sequi sit soluta voluptas voluptatum. Ab
+                    adipisci aut excepturi ipsam modi, nemo nesciunt, nobis pariatur placeat quae ratione rem tempore!
+                    Adipisci alias beatae corporis delectus dignissimos est excepturi facere inventore labore laborum
+                    nobis non officia perferendis placeat qui recusandae repellendus sunt unde, veniam voluptas. A ea et
+                    eum illum in incidunt obcaecati unde! A ab aliquam blanditiis cum deleniti dolore eveniet excepturi
+                    hic ipsam ipsum itaque labore nobis obcaecati provident quaerat, rerum sint sunt tempore, temporibus
+                    voluptatibus. Animi autem cupiditate delectus deleniti ea eos est et facilis in laboriosam maiores,
+                    minus natus nemo nisi quasi qui quidem sint, suscipit vel veniam! Blanditiis, corporis cupiditate
+                    dolorum esse eum illo necessitatibus numquam possimus similique? Ab, sapiente, voluptatibus.
+                </p>
+            </div>
+        </div>
         @include('components.catalog.product.mini-list', ['title' => 'Best Seller', 'products' => $bestsellers])
     </main>
 @endsection
