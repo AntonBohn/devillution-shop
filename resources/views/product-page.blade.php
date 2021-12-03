92c28525295d369088e9cfa13d5a55d2bdd8241c
AntonBohn <anton.bohn@tudock.de>
2021-12-03 11:19:49 +0100

    feat(product-page): implemented the product-page

--- resources/views/product-page.blade.php
+++ resources/views/product-page.blade.php
@@ -2,6 +2,19 @@
 
 @section('content')
     <div class="product-page">
-        @include('catalog.product.list.tile')
+        <div class="product-image">
+            <img src="{{$product->images}}" alt="">
+        </div>
+        <div class="product-page-name">
+            {{ $product->name }}
+        </div>
+        @include('catalog.product.price')
+        <div class="short-description-text">
+            {{ $product->ShortDescription }}
+        </div>
+        <div class="description-text">
+            {{ $product->description }}
+        </div>
     </div>
+    {{--    @include('catalog.product.add-to-cart')--}}
 @endsection
